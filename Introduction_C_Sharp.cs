4c072e1f536c4012a1c559294e037ced5fd00db7
Manobalan04 <manobalans2001@outlook.com>
2024-08-20 20:32:55 +0530

    first commit

---
+++ Introduction_C_Sharp.cs
@@ -0,0 +1,21 @@
+﻿namespace Introduction_C_Sharp
+    {
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, T;
+            x = 3;
+            y = 5;
+            T = x+y;
+            Console.WriteLine("Total: "+T);
+            Console.WriteLine($"Total:{ T}");
+
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
+
