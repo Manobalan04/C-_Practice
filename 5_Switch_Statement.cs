5adeefd294ae8d61d6c07dac5aa06ab68d172d39
Manobalan04 <manobalans2001@outlook.com>
2024-08-21 22:20:25 +0530

    first commit

---
+++ 5_Switch_Statement.cs
@@ -0,0 +1,54 @@
+﻿namespace Introduction_C_Sharp
+    {
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int CurrentRate, PreviousRate, Unit, Rate, Amount;
+            char Type;
+
+            Console.Write("Enter CurrentRate: ");
+            CurrentRate = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter PreviousRate: ");
+            PreviousRate = int.Parse(Console.ReadLine());
+
+            Unit = Math.Abs(CurrentRate - PreviousRate);
+            Console.WriteLine("Total Unit Used: "+Unit);
+
+            Console.Write("Enter Connection Type: ");
+            Type = Convert.ToChar(Console.Read());
+
+            switch (Type)
+            {
+                case 'D':
+                    Rate = 2;
+                    break;
+                case 'I':
+                    Rate = 6;
+                    break;
+                case 'C':
+                    Rate = 4;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Type");
+                    Rate = 0;
+                    break;
+            }
+
+            Amount = Rate * Unit;
+            Console.WriteLine("Amount= "+Amount);
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
